9f12da8139b88ef5adfcca6320c0542bac065221
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-17 21:06:20 +0100

    creata funzione per aggiungere i membri

--- myscript.js
+++ myscript.js
@@ -2,31 +2,64 @@ const team = [
     {
       name: 'Wayne Barnett',
       role: 'Founder & CEO',
-      image: 'wayne-barnett-founder-ceo.jpg',
+      image: 'img/wayne-barnett-founder-ceo.jpg',
     },
     {
       name: 'Angela Caroll',
       role: 'Chief Editor',
-      image: 'angela-caroll-chief-editor.jpg',
+      image: 'img/angela-caroll-chief-editor.jpg',
     },
     {
       name: 'Walter Gordon',
       role: 'Office Manager',
-      image: 'walter-gordon-office-manager.jpg',
+      image: 'img/walter-gordon-office-manager.jpg',
     },
     {
       name: 'Angela Lopez',
       role: 'Social Media Manager',
-      image: 'angela-lopez-social-media-manager.jpg',
+      image: 'img/angela-lopez-social-media-manager.jpg',
     },
     {
       name: 'Scott Estrada',
       role: 'Developer',
-      image: 'scott-estrada-developer.jpg',
+      image: 'img/scott-estrada-developer.jpg',
     },
     {
       name: 'Barbara Ramos',
       role: 'Graphic Designer',
-      image: 'barbara-ramos-graphic-designer.jpg',
+      image: 'img/barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+//definisco le variabili
+  const bottone = document.getElementById("addMemberButton");
+  console.log(bottone);
+  const teamContainer = document.querySelector(".team-container");
+  console.log(teamContainer);
+  
+  
+//creo una funzione per aggiungere i membri 
+function aggiungiMembro() {
+    teamContainer.innerHTML += `
+    <div class="team-card">
+        <div class="card-image">
+            <img
+                src="${team[i].image}"
+                alt=""
+            />
+        </div>
+        <div class="card-text">
+            <h3>${team[i].name}</h3>
+            <p>${team[i].role}</p>
+        </div>
+    </div>
+    `
+}
+
+
+  
+
+
+
+  
+    
